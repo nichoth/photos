180ead6b56eab3fb3c93bb55adbd43a137ce467d
nichoth <nichoth@gmail.com>
2021-03-12 13:20:06 -0800

    starting

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { html } from 'htm/preact'
+import { render } from 'preact'
+var Bus = require('@nichoth/events')
+var OnRoute = require('route-event')
+
+function photos () {
+    var bus = Bus({
+        memo: true
+    })
+    var emit = bus.emit.bind(bus)
+
+    var _html = html`<p>fooooooo</p>`
+
+    var onRoute = OnRoute()
+
+    onRoute(path => emit(evs.route.change, path))
+    var { setRoute } = onRoute
+
+    render(_html, document.getElementById('content'))
+
+    return { bus, setRoute }
+}
+
+module.exports = photos
